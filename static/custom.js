cd8a96e778f6bc051afcbd7bf5cce17edbf085a0
Ryan Mehri <ryan.mehri1@gmail.com>
2020-01-12 12:29:51 -0800

    Finalize ui

--- static/custom.js
+++ static/custom.js
@@ -30,7 +30,11 @@ $("#target").on("submit", function(e) {
   // loading
   $(".chat-container").append(`
         <div class="chat-message text-center col-md-2 offset-md-10 bot-message" id="loading">
-            <b>...</b>
+          <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+          </div>
         </div>
     `);
 
